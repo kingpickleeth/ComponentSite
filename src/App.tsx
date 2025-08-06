3fc76ff4fcf08350bbf28e0eab94fb30dc174fb4
Charlie <charlie@Charlies-Mac-mini.local>
2025-08-06 06:22:46 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>🧩 My React Component Library</h1>
+    </div>
+  );
+}
+
+export default App;
